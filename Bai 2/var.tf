7c70995ed4bc34a85106dd060f81c235bdfbf17f
chaudt246 <110970552+chaudt246@users.noreply.github.com>
2022-10-05 15:19:00 +0700

    update bai 2

---
+++ Bai 2/var.tf
@@ -0,0 +1,11 @@
+variable "stage" {
+  description = "stage of code"
+  type        = string
+  default     = "chau"
+}
+
+variable "keyname" {
+  description = "Name of keypair"
+  type        = string
+  default     = "keypair"
+}
